9cd71ac626b34f3c252d04fa946f78c5482bac03
André Alves Barbosa <andrealvesbarbosa@hotmail.com>
2026-01-17 15:49:10 -0300

    Changing arguments to run on a Linux server

--- chat_bot.js
+++ chat_bot.js
@@ -14,7 +14,17 @@ const client = new Client({
     },
     puppeteer: {
         headless: true,
-        args: ['--no-sandbox', '--disable-setuid-sandbox']
+        // ARGUMENTOS OBRIGATÓRIOS PARA RODAR NO RAILWAY/LINUX:
+        args: [
+            '--no-sandbox',
+            '--disable-setuid-sandbox',
+            '--disable-dev-shm-usage',
+            '--disable-accelerated-2d-canvas',
+            '--no-first-run',
+            '--no-zygote',
+            '--single-process',
+            '--disable-gpu'
+        ]
     }
 });
 
